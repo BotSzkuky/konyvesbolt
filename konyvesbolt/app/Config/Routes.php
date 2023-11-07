f9162309006be0c21aa6ae5bbfef7515ff1962e9
bistvan4 <bencze780@gmail.com>
2023-11-07 18:27:02 +0100

    halgatóknak otthono feladat

--- konyvesbolt/app/Config/Routes.php
+++ konyvesbolt/app/Config/Routes.php
@@ -6,4 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/udv', 'Home::udv');
+$routes->get('/welcome_message', 'Home::udv');
--- konyvesbolt/app/Controllers/Home.php
+++ konyvesbolt/app/Controllers/Home.php
@@ -11,7 +11,7 @@ class Home extends BaseController
     }
 
 
-    public function udv()
+    public function welcome_message()
     {
         return view('udv');
 
